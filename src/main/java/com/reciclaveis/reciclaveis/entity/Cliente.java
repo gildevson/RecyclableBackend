2505891593865145f5054aba69f2bce84bf86ca9
gildevson@gmail.com <gilsonfonseca92@gmail.com>
2025-08-13 22:24:03 -0300

    Adicionando model cliente

--- src/main/java/com/reciclaveis/reciclaveis/entity/Cliente.java
+++ src/main/java/com/reciclaveis/reciclaveis/entity/Cliente.java
@@ -1,8 +1,25 @@
 package com.reciclaveis.reciclaveis.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@Entity(name = "cliente") // é utilizando para criar tabela de acordo com jpa
 public class Cliente {
-    private String nomeCliente;
-    private String cnpj;
-    private String cpf;
-    
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String clienteNome;
+    private String clienteCnpj;
+    private String clienteCpf;
+    private String clienteTelefone;
+    private String clienteEmail;
+    private String clienteCelular;
+    private String cliente;
 }
