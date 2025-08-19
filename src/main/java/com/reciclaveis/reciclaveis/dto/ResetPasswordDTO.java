28e7c7161fe6de71ea90e705cdc91571ba24bc62
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2025-08-19 13:48:44 -0300

    Adicionando sintaxe

--- src/main/java/com/reciclaveis/reciclaveis/dto/ResetPasswordDTO.java
+++ src/main/java/com/reciclaveis/reciclaveis/dto/ResetPasswordDTO.java
@@ -1,31 +1,31 @@
 package com.reciclaveis.reciclaveis.dto;
 
 public class ResetPasswordDTO {
-   private String token;
-   private String password;
-   private String confirmPassword;
+    private String token;
+    private String password;
+    private String confirmPassword;
 
-   public String getToken() {
-      return token;
-   }
+    public String getToken() {
+        return token;
+    }
 
-   public void setToken(String token) {
-      this.token = token;
-   }
+    public void setToken(String token) {
+        this.token = token;
+    }
 
-   public String getPassword() {
-      return password;
-   }
+    public String getPassword() {
+        return password;
+    }
 
-   public void setPassword(String password) {
-      this.password = password;
-   }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-   public String getConfirmPassword() {
-      return confirmPassword;
-   }
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
 
-   public void setConfirmPassword(String confirmPassword) {
-      this.confirmPassword = confirmPassword;
-   }
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
--- src/main/java/com/reciclaveis/reciclaveis/repository/ClienteRepository.java
+++ src/main/java/com/reciclaveis/reciclaveis/repository/ClienteRepository.java
@@ -1,9 +1,7 @@
 package com.reciclaveis.reciclaveis.repository;
 
-
 import com.reciclaveis.reciclaveis.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
